a043257a5213a502197df32030bb89b9906bddaa
alsu <alsu@c02zc54jlvdl.windows.ebi.ac.uk>
2020-02-11 14:46:09 +0000

    parallel

---
+++ parallel__calculateStat__eachCell/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/spatial/MER_hypothalamic/hypothalamicSpatialScripts/parallel__calculateStat__eachCell/parallel__calculateStat__eachCell.Rmd")
---
+++ parallel__calculateStat__eachCell/sub_rmd.sh
@@ -0,0 +1,23 @@
+#INITIALISE FOLDERS
+my_folder=/nfs/research1/marioni/alsu
+out_folder=${my_folder}/clust_out/spatial
+err_folder=${my_folder}/clust_err/spatial
+
+#SELECT SCRIPT
+#If you change this, you MUST update the wrapper's grep
+script_name=par_calcStat
+
+#CHOOSE PARAMETERS
+#RAM in megabytes
+memory=500000
+r_command="rusage[mem=${memory}]"
+#num_processors
+nproc=20
+
+smg=/nfs/research1/marioni/alsu/singularity/R1.simg
+script=/nfs/research1/marioni/alsu/spatial/MER_hypothalamic/hypothalamicSpatialScripts/parallel__calculateStat__eachCell/run_rmd.R
+
+bsub -q research-rh74 -e ${err_folder}/${script_name} \
+-o ${out_folder}/${script_name} -P bigmem \
+-M $memory -R $r_command -n $nproc -J ${script_name} \
+"singularity exec $smg Rscript $script"
